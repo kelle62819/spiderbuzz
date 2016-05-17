d89c0f1a1719409e9d00e4a11e38626a347396a8
Jeff <jeff@blindvalet.com>
2016-05-17 17:51:00 +0000

    added sounds on buttons

--- run.py
+++ run.py
@@ -14,17 +14,29 @@ def main():
 
     buzz.lightRun(devices)
     buzz.lightRun(devices)
+    buzz.lightRun(devices)
+    buzz.lightRun(devices)
     while 1:
     	click = buzz.getPress(devices)
     	if click%5 == 0:  ## buzz button was clicked
     		clicker=click/5
     		sons.play("ringin.mp3")
     		buzz.light(devices,clicker,1)
-    		time.sleep(4)
+    		time.sleep(5)
     		buzz.light(devices,clicker,0)
     		buzz.clearPresses(devices);
-    			
-
+    	if click%5 == 4:  ## yellow button was clicked
+            sons.play("dingdong.mp3")
+        if click%5 == 3:  ## green button was clicked
+            sons.play("right.mp3")
+        if click%5 == 2:  ## orange button was clicked
+            sons.play("wrong.mp3")
+        if click%5 == 1:  ## blue button was clicked
+            sons.play("generique.mp3")
+            buzz.lightRun(devices)
+            buzz.lightRun(devices)
+            buzz.lightRun(devices)
+            buzz.lightRun(devices)
 
 
 if __name__ == '__main__': main()
